626dc2361d09cc2e228694fdc4053bb31e2dea36
John <codebeast0420@gmail.com>
2022-12-20 18:03:51 +0800

    bottom header link

--- components/header.js
+++ components/header.js
@@ -12,7 +12,7 @@ const Header = ({ intro, setIntro }) => {
             <ul className="flex flex-col p-4 mt-4  rounded-lg bg-inherit md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-inherit ">
               <li>
                 <a
-                  href="#"
+                  href="/"
                   className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-white dark:bg-blue-600 md:dark:bg-transparent"
                   aria-current="page"
                 >
@@ -21,7 +21,7 @@ const Header = ({ intro, setIntro }) => {
               </li>
               <li>
                 <a
-                  href="#"
+                  href="/aboutUs"
                   className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                 >
                   ABOUT US
--- components/speaker.js
+++ components/speaker.js
@@ -10,8 +10,8 @@ const Speaker = ({ img, name, content }) => {
 					<p className="text-[#475060]" style={{ fontFamily: "Jost" }}>{content}</p>
 				</div>
 			</div>
-			<div className="flex ml-[90px] items-center">
-				<p className="text-[#1CCADF]">Links To Site</p>
+			<div className="flex ml-[90px] items-center cursor-pointer">
+				<a className="text-[#1CCADF]">Links To Site</a>
 				<svg width="17" height="12" viewBox="0 0 17 12" fill="none" xmlns="http://www.w3.org/2000/svg" className="ml-[10px]">
 					<path d="M10.2938 11.5167L10.6639 11.8835C10.8205 12.0388 11.0739 12.0388 11.2289 11.8835L16.8825 6.28086C17.0392 6.12556 17.0392 5.87444 16.8825 5.72079L11.2289 0.116475C11.0722 -0.038826 10.8189 -0.038826 10.6639 0.116475L10.2938 0.483252C10.1355 0.640204 10.1388 0.896286 10.3005 1.04993L14.7225 5.33915L0.400004 5.33915C0.178335 5.33915 0 5.51592 0 5.73566L0 6.26434C0 6.48408 0.178335 6.66086 0.400004 6.66086L14.7225 6.66086L10.3005 10.9501C10.1372 11.1037 10.1338 11.3598 10.2938 11.5167Z" fill="#1CCADF" />
 				</svg>
