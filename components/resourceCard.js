e1bcb2e08faabda78330aa6fe149cf720cbb5fd5
John <codebeast0420@gmail.com>
2022-12-20 18:50:26 +0800

    fifth page first

---
+++ components/resourceCard.js
@@ -0,0 +1,26 @@
+import Image from "next/image";
+
+const ResourceCard = ({ img, title, content, children }) => {
+	return (
+		<div className="w-[32%] rounded-[10px] p-[5px]" style={{ boxShadow: "0px 10px 40px rgba(7, 24, 50, 0.07)" }}>
+			<Image src={img} width={370} height={265} alt={"Resource Card"} />
+			<div className="mt-[10px] pl-[15px] pr-[15px] pb-[30px]">
+				<div className="flex">
+					{children}
+				</div>
+				<div className="mt-[20px]">
+					<p className="text-black font-bold capitalize">{title}</p>
+					<p className="text-[#475060] non-italic font-medium" style={{ fontFamily: "Jost" }}>{content}</p>
+					<a href="#" className="mt-[40px] text-[#1CCADF] flex items-center">
+						<p>More Information</p>
+						<svg width="18" height="12" viewBox="0 0 18 12" fill="none" xmlns="http://www.w3.org/2000/svg" className="ml-[20px]">
+							<path d="M11.2443 0.483251L11.617 0.116476C11.7748 -0.0388253 12.03 -0.0388253 12.1862 0.116476L17.8816 5.71914C18.0395 5.87444 18.0395 6.12556 17.8816 6.27921L12.1862 11.8835C12.0284 12.0388 11.7731 12.0388 11.617 11.8835L11.2443 11.5167C11.0848 11.3598 11.0881 11.1037 11.251 10.9501L15.7056 6.66085L0.402962 6.66085C0.179654 6.66085 0 6.48408 0 6.26434L0 5.73566C0 5.51592 0.179654 5.33914 0.402962 5.33914L15.7056 5.33914L11.251 1.04993C11.0864 0.896285 11.0831 0.640204 11.2443 0.483251Z" fill="#1CCADF" />
+						</svg>
+					</a>
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default ResourceCard;
---
+++ pages/resources.js
@@ -0,0 +1,56 @@
+import Bottom from "../components/bottom";
+import CommonBtn from "../components/commonBtn";
+import Header from "../components/header";
+import ResourceCard from "../components/resourceCard";
+import TopTitle from "../components/topTitle";
+
+const Resources = () => {
+	const title = "Introduction to the Problems and Power of Social Media";
+	const content = "This session is an overview session to ensure all students understand how social media platforms are designed and leveraged for good or bad purposes."
+	return (
+		<div>
+			<Header />
+			<TopTitle title={"Resources"} />
+			<div className="relative z-[11] bg-white pl-[10%] pr-[10%] mt-[20px]">
+				<div>
+					<p className="text-[#142630] font-bold non-italic text-2xl">Tags:</p>
+					<div className="flex ">
+						<CommonBtn text={"Video"} />
+						<CommonBtn text={"Article"} bgColor={"bg-[#e1e4ea]"} className={"ml-[10px]"} />
+						<CommonBtn text={"Session1"} bgColor={"bg-[#e1e4ea]"} className={"ml-[10px]"} />
+						<CommonBtn text={"Session2"} bgColor={"bg-[#e1e4ea]"} className={"ml-[10px]"} />
+						<CommonBtn text={"Session3"} bgColor={"bg-[#e1e4ea]"} className={"ml-[10px]"} />
+						<CommonBtn text={"Session4"} bgColor={"bg-[#e1e4ea]"} className={"ml-[10px]"} />
+					</div>
+				</div>
+				<div className="mb-[50px]">
+					<div className="flex mt-[50px] justify-between">
+						<ResourceCard title={title} content={content} img={"/src/img/resouce_card_1.png"}>
+							<CommonBtn text={"Article"} bgColor={"bg-[#e1e4ea]"} />
+							<CommonBtn text={"Video"} bgColor={"bg-[#e1e4ea]"} className={"ml-[10px]"} />
+						</ResourceCard>
+						<ResourceCard title={title} content={content} img={"/src/img/resouce_card_2.png"}>
+							<CommonBtn text={"Session1"} bgColor={"bg-[#e1e4ea]"} />
+						</ResourceCard>
+						<ResourceCard title={title} content={content} img={"/src/img/resouce_card_3.png"}>
+							<CommonBtn text={"Article"} bgColor={"bg-[#e1e4ea]"} />
+						</ResourceCard>
+					</div>
+					<div className="flex mt-[50px] justify-between">
+						<ResourceCard title={title} content={content} img={"/src/img/resouce_card_4.png"}>
+							<CommonBtn text={"Video"} bgColor={"bg-[#e1e4ea]"} /> </ResourceCard>
+						<ResourceCard title={title} content={content} img={"/src/img/resouce_card_5.png"}>
+							<CommonBtn text={"Article"} bgColor={"bg-[#e1e4ea]"} /> </ResourceCard>
+						<ResourceCard title={title} content={content} img={"/src/img/resouce_card_6.png"}>
+							<CommonBtn text={"Article"} bgColor={"bg-[#e1e4ea]"} />
+							<CommonBtn text={"Video"} bgColor={"bg-[#e1e4ea]"} className={"ml-[10px]"} />
+						</ResourceCard>
+					</div>
+				</div>
+			</div>
+			<Bottom />
+		</div>
+	)
+}
+
+export default Resources;
