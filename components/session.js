4a0616576e1a909b08db64bbd75a04cb4581cb55
John <codebeast0420@gmail.com>
2022-12-26 10:51:21 +0800

    session blue bar delete

--- components/session.js
+++ components/session.js
@@ -77,50 +77,10 @@ const Session = () => {
 									/>
 								</SwiperSlide>
 							))}
-							{/* <SwiperSlide>
-								<SessionCard
-
-									src={sessions[1] ? sessions[1].banner.url : ""}
-									title={sessions[1] ? sessions[1].title : ""}
-									text={sessions[1] ? documentToReactComponents(sessions[1].description.json) : ""}
-									date={sessions[1] ? convertDate(sessions[1].startTime) : ""}
-									time={"03.00PM - 05:00AM ET"}
-								/>
-							</SwiperSlide>
-							<SwiperSlide>
-								<SessionCard
-									src={sessions[0].banner ? sessions[0].banner.url : ""}
-									title={sessions[0] ? sessions[0].title : ""}
-									text={"This session is designed to acknowledge that students may be interested in different approaches to engaging on social media. Some may want to take on a public, outspoken role while others..."}
-									date={"February 26, 2023"}
-									time={"03.00PM - 05:00AM ET"}
-								/>
-							</SwiperSlide> */}
-							{/* <SwiperSlide>
-								<SessionCard
-									src={"/src/img/test.png"}
-									title={"Who Can You Trust?"}
-									text={"This session is designed to teach students how to discern trust from lies and how to identify malicious content that may be disguised. They will learn how to assess content to determine its validity as well as..."}
-									date={"March 5, 2023"}
-									time={"03.00PM - 05:00AM ET"}
-								/>
-							</SwiperSlide> */}
 						</Swiper>
 					</div>
-					{/* <SlideBtn /> */}
 					<div className="flex mt-[50px]" >
-						<div className="flex mt-[20px]">
-							<svg width="40" height="7" viewBox="0 0 40 7" fill="none" xmlns="http://www.w3.org/2000/svg">
-								<rect width="40" height="7" rx="3.5" fill="#1CCADF" />
-							</svg>
-							<svg className="ml-[10px]" width="40" height="7" viewBox="0 0 40 7" fill="none" xmlns="http://www.w3.org/2000/svg">
-								<rect opacity="0.3" width="40" height="7" rx="3.5" fill="#475060" />
-							</svg>
-							<svg className="ml-[10px]" width="40" height="7" viewBox="0 0 40 7" fill="none" xmlns="http://www.w3.org/2000/svg">
-								<rect opacity="0.3" width="40" height="7" rx="3.5" fill="#475060" />
-							</svg>
-						</div>
-						<div className="flex ml-[70%] ">
+						<div className="flex ml-[85%] ">
 							<svg width="55" height="41" viewBox="0 0 55 41" fill="none" xmlns="http://www.w3.org/2000/svg" className="prev-button cursor-pointer">
 								<rect opacity="0.1" width="55" height="41" rx="4" fill="#475060" />
 								<path d="M24.383 12.6846L23.8653 12.165C23.6461 11.945 23.2916 11.945 23.0747 12.165L15.1644 20.1021C14.9452 20.3221 14.9452 20.6779 15.1644 20.8955L23.0747 28.835C23.294 29.055 23.6484 29.055 23.8653 28.835L24.383 28.3154C24.6045 28.093 24.5998 27.7303 24.3736 27.5126L18.1866 21.4362H39.4403C39.7505 21.4362 40 21.1858 40 20.8745V20.1255C40 19.8142 39.7505 19.5638 39.4403 19.5638H18.1866L24.3736 13.4874C24.6022 13.2697 24.6068 12.907 24.383 12.6846Z" fill="#475060" />
